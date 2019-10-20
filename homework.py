99018bd06cc2f3da6733f936a9a0684e7ed1fb77
Piotr Ziętara <zietara89@gmail.com>
2019-10-20 21:57:28 +0200

    code optimization

--- homework.py
+++ homework.py
@@ -16,20 +16,19 @@ class Homework(object):
                 return 'Tak'
 
 
-    def val_pesel(self, num: int) -> bool:  #spróbować zsumowaną liczbę weryfikować przez modulo 10 (%10)
+    def val_pesel(self, num: int) -> bool:
         temp_num = str(num)
 
-        temp_list = [int(i) for i in temp_num] #list of num each digit
-        multiple_by = [1, 3, 7, 9, 1, 3, 7, 9, 1, 3, 1] #pattern
-        result = [temp_list * multiple_by for temp_list, multiple_by in zip(temp_list, multiple_by)] # multiple 2 lists
+        temp_list = [int(i) for i in temp_num] # list of digits of a given number
+        multiple_by = [1, 3, 7, 9, 1, 3, 7, 9, 1, 3, 1] # pattern
+        result = [temp_list * multiple_by for temp_list, multiple_by in zip(temp_list, multiple_by)] # multiply 2 lists
 
-        temp_num_2 = str(sum(result)) #return sum of the list 'result' and convert to string
-        temp_list_2 = [int(i) for i in temp_num_2] #create list with digits from temp_num_2 number
+        temp_num_2 = sum(result)
 
-        if temp_list_2[-1] == 0: #prawidłowy
-            return 'D'
+        if temp_num_2 % 10 == 0:
+            return 'D'  # 'D' means PESEL is valid
         else:
-            return 'N' #Nieprawidłowy
+            return 'N'  # 'N' PESEL is incorrect
 
 h = Homework()
 h.val_pesel(44051401458)
