aaefacad1deac1ec18a065fede6bd975026be0c2
Piotr Ziętara <zietara89@gmail.com>
2019-10-20 22:00:47 +0200

    code optimization

--- homework_test.py
+++ homework_test.py
@@ -1,19 +1,28 @@
 import unittest
 from homework import Homework
 
+
 class HomeworkTestCase(unittest.TestCase):
-    def test_prime_num(self):
+
+    def test_prime_num_positive(self):
         prim = Homework()
 
         self.assertEqual(prim.prime_num(13), 'Tak')
+
+    def test_prime_num_negative(self):
+        prim = Homework()
+
         self.assertEqual(prim.prime_num(6), 'Nie')
 
-    def test_val_pesel(self):
+    def test_val_pesel_positive(self):
         pes = Homework()
 
         self.assertEqual(pes.val_pesel(44051401458), 'D')
-        self.assertEqual(pes.val_pesel(12345678912), 'N')
 
+    def test_val_pesel_negative(self):
+        pes = Homework()
+
+        self.assertEqual(pes.val_pesel(12345678912), 'N')
 
 if __name__ == '__main__':
     unittest.main()
